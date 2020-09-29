e757eb0e8bf7aa10bac32f66d511a2b0b9d58d3c
iii <miselrkba@gmail.com>
2020-09-29 20:12:22 +0200

    Updates

---
+++ precache-manifest.678c04b725b7cfba23853a1ed2f13e03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bea7efdcf2d360a8d358c592b0cd8f8",
+    "url": "/leads/index.html"
+  },
+  {
+    "revision": "5a5e66b28f66aacbf53c",
+    "url": "/leads/static/css/main.70528560.chunk.css"
+  },
+  {
+    "revision": "789ed7930430f5047c04",
+    "url": "/leads/static/js/2.8d05fcc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leads/static/js/2.8d05fcc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5e66b28f66aacbf53c",
+    "url": "/leads/static/js/main.2cf54396.chunk.js"
+  },
+  {
+    "revision": "412bd36b4e17601d0a6a",
+    "url": "/leads/static/js/runtime-main.c608d309.js"
+  },
+  {
+    "revision": "09f185c8cdafba917dfad9c9461d1c45",
+    "url": "/leads/static/media/6.09f185c8.jpg"
+  }
+]);
